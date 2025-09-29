3aa3784b36761c6f395925a6b53c8f7b583e3e7d
Tanvi Aher <ahertanvi462@gmail.com>
2025-09-29 16:31:44 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './views/Home/Home.css';
+import Home from './views/Home/Home.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Home />);
